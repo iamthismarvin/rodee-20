d2df81eb324a6e43aeb0b7d06d942e66edc8e4e6
Marvin Parada <hello@marvin.dev>
2020-08-28 12:39:32 -0700

    NEW: Install Prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,9 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   rules: {
+    'prettier/prettier': ['error'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 };
