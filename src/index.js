47f3c5bcd34bb0731b9e7c4bae4cf9a640f03503
Marvin Parada <hello@marvin.dev>
2020-08-29 14:24:23 -0700

    NEW: Create first Rodee-20 command.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 
-console.log(Discord);
+const client = new Discord.Client();
+const prefix = '!';
+
+client.on('message', (message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === 'rodee20') {
+    message.reply(`Hello, I am Rodee-20! A friendly dice roll discord bot.`);
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
